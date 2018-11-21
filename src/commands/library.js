24e1b4567fe7e42f6458bd20c0f090e66d85bbc0
Morgan Laco <morganlaco@gmail.com>
2018-11-21 10:08:41 -0500

    Extract getLibraryComponent method

--- src/commands/library.js
+++ src/commands/library.js
@@ -12,7 +12,6 @@
  * defaults to infinitered
  */
 
-const LIBRARY_INDEX_GIST = 'fd8513a906c8da31984c68064f0498e6'
 const LIBRARY_INDEX_URL = `https://raw.githubusercontent.com/{parameters.options.github || 'infinitered'}/ignite-library/master/library.json`
 
 const FAKE_TEMPLATE = `
@@ -51,10 +50,12 @@ export function <%= props.pascalName %>(props: <%= props.pascalName %>Props) {
 }
 `
 
+const { terminal: term, ScreenBuffer } = require( 'terminal-kit' )
 const Gists = require('gists');
 const gists = new Gists({
   token: process.env['IGNITE_GITHUB_TOKEN']
 });
+const getLibraryComponent = require('../lib/getLibraryComponent')
 
 module.exports = async function (context) {
   const { parameters, filesystem, print, prompt, system } = context
@@ -64,36 +65,7 @@ module.exports = async function (context) {
   switch (parameters.second.toLowerCase()) {
     case 'search':
       // go out and get the Infinite Red component library
-      let res
-      try {
-        res = await gists.get(LIBRARY_INDEX_GIST)
-      } catch (e) {
-        // if not found, exit gracefully 
-        print.info(`Can't find the Ignite library index at (https://gist.github.com/jamonholmgren/${LIBRARY_INDEX_GIST})`)
-        print.info('')
-        print.info(`You may have set your env variable incorrectly (Not really, this is COMING SOON) or don't have a Personal Access Token set (more likely!)`)
-        print.info(`Go to https://github.com/settings/tokens to generate one and then run this in terminal:`)
-        print.info(`export IGNITE_GITHUB_TOKEN='tokenhere'`)
-        process.exit(e.code)
-      }
-
-      // parse out the components
-      const components = JSON.parse(res.body.files['library-index.json'].content).components
-
-      // search for a component
-      const searchResults = components.filter(comp => comp.name.includes(filename))
-
-      // console.dir(searchResults)
-      const selection = await prompt.ask({
-        type: 'list',
-        name: 'component',
-        message: `Components matching ${filename}:`,
-        choices: searchResults.map(r => r.name + " - " + r.description)
-      })
-
-      const selectedResult = searchResults.find(r => (r.name + " - " + r.description) === selection.component)
-
-      const selectedGist = await gists.get(selectedResult.gist)
+      const selectedGist = await getLibraryComponent(context, filename)
 
       // have a new gist, now fill out the template
       print.info('')
---
+++ src/lib/getLibraryComponent.js
@@ -0,0 +1,47 @@
+/**
+ * Retrieves a Library component from GitHub Gists
+ */
+const LIBRARY_INDEX_GIST = '4fe0dfd70e7556f62cccd24c96a06be2'//'fd8513a906c8da31984c68064f0498e6'
+const Gists = require('gists');
+const gists = new Gists({
+  token: process.env['IGNITE_GITHUB_TOKEN']
+});
+
+async function getLibraryComponent (context, filename) {
+  const { print, prompt } = context
+
+  let res
+  try {
+    res = await gists.get(LIBRARY_INDEX_GIST)
+  } catch (e) {
+    // if not found, exit gracefully 
+    print.info(`Can't find the Ignite library index at (https://gist.github.com/jamonholmgren/${LIBRARY_INDEX_GIST})`)
+    print.info('')
+    print.info(`You may have set your env variable incorrectly (Not really, this is COMING SOON) or don't have a Personal Access Token set (more likely!)`)
+    print.info(`Go to https://github.com/settings/tokens to generate one and then run this in terminal:`)
+    print.info(`export IGNITE_GITHUB_TOKEN='tokenhere'`)
+    process.exit(e.code)
+  }
+
+  // parse out the components
+  const components = JSON.parse(res.body.files['library-index.json'].content).components
+
+  // search for a component
+  const searchResults = components.filter(comp => comp.name.includes(filename))
+
+  // console.dir(searchResults)
+  const selection = await prompt.ask({
+    type: 'list',
+    name: 'component',
+    message: `Components matching ${filename}:`,
+    choices: searchResults.map(r => r.name + " - " + r.description)
+  })
+
+  const selectedResult = searchResults.find(r => (r.name + " - " + r.description) === selection.component)
+
+  const selectedGist = await gists.get(selectedResult.gist)
+
+  return selectedGist
+}
+
+module.exports = getLibraryComponent
