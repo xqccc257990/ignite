6438d06c681b5e641b61d81c9b164f68b8f15d46
Morgan Laco <morganlaco@gmail.com>
2018-11-24 09:05:16 -0500

    Fix template entry site advancement

--- src/lib/completeLibraryTemplate.js
+++ src/lib/completeLibraryTemplate.js
@@ -6,11 +6,15 @@ async function completeLibraryTemplate (context, content) {
   const { print } = context
   const { terminal: term, ScreenBuffer } = require( 'terminal-kit' )
   var screen = new ScreenBuffer({dst: term, noFill: false})
+  var templateState = {
+    content: content,
+    workingContent: content,
+    currentSite: 1
+  }
 
   function placeCursor (content, currentEntryPoint) {
     // return content
     var regex = new RegExp('\\$' + currentEntryPoint, "gm")
-    print.info(currentEntryPoint)
     if (content.search(regex)) {
       return content.replace(regex, "▋")
     }
@@ -21,10 +25,17 @@ async function completeLibraryTemplate (context, content) {
     setTimeout( function() { process.exit() } , 100 ) ;
   }
 
+  function drawTemplate(content) {
+    term.clear()
+    term(content)
+    screen.draw()
+  }
+
   async function templateLoopIteration(templateState, name = '') {
+    print.info(JSON.stringify(templateState))
+
     var nextSite = templateState.currentSite
     var content = templateState.content
-    print.info(JSON.stringify(templateState))
     var workingContent = templateState.content
 
     if ( name === 'CTRL_C' ) { terminate() ; }
@@ -35,9 +46,7 @@ async function completeLibraryTemplate (context, content) {
     workingContent = placeCursor(content, nextSite)
     if(!workingContent) { terminate() ; }
 
-    term.clear()
-    term(workingContent)
-    screen.draw()
+    drawTemplate(workingContent)
 
     return {
       content: content,
@@ -46,26 +55,15 @@ async function completeLibraryTemplate (context, content) {
     }
   }
 
-  var templateState = {
-    content: content,
-    workingContent: content,
-    currentSite: 1
-  }
-
-  term.grabInput( ) ;
-  Object.assign(templateState, templateLoopIteration(templateState, ''))
-
-  term.on( 'key' , function( name , matches , data ) {
-    Object.assign(templateState, templateLoopIteration(templateState, name))
-    // print.info(JSON.stringify(templateState))
+  const newTemplateState = await templateLoopIteration(templateState)
+  Object.assign(templateState, newTemplateState)
+  drawTemplate(templateState.workingContent)
+  
+  term.grabInput( );
+  term.on( 'key' , async function( name , matches , data ) {
+    const newTemplateState = await templateLoopIteration(templateState, name)
+    Object.assign(templateState, newTemplateState)
   } ) ;
-
-
-  // for (var i=0; i<lines.length; i++) {
-  //   line = lines[i]
-  //   res = re.exec(line)
-  //   print.info(line)
-  // }
 }
 
 module.exports = completeLibraryTemplate
